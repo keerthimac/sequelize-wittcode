3339dcd0de422c03adc0c338dd9598c460627b6e
Darshana <keerthi.mac@gmail.com>
2022-01-02 20:58:24 +0530

    Basic Auth,Define,Sync Method

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const Sequelize = require("sequelize"); //constructor 
+const Sequelize = require("sequelize"); //constructor
 const mysql = require("mysql2");
 
-
 //Create new instant from class
-const sequelize = new Sequelize('sequelize_test', 'root', '0542222175', {
-    //host:localhost, //Default Value
-    //port: 3306, //Default Value
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize("sequelize_test", "root", "0542222175", {
+  //host:localhost, //Default Value
+  //port: 3306, //Default Value
+  dialect: "mysql",
+});
 
 // async function myFunction() {
 //     await sequelize.authenticate()
@@ -16,40 +15,60 @@ const sequelize = new Sequelize('sequelize_test', 'root', '0542222175', {
 
 // myFunction();
 
-
 //authenticate to database
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => {
+    console.log("error connecting to database");
+  });
 
-sequelize.authenticate().then(() => {
-    console.log('connection successful')
-}).catch((err) => {
-    console.log("error connecting to database")
-})
+//sync all the table in database (OPTIONAL)
+// sequelize
+//   .sync({ alter: true })
+//   .then(() => {
+//     console.log("database sync");
+//   })
+//   .catch((err) => {
+//     console.log("error in database sync");
+//   });
 
-const User = sequelize.define('user', {
+//Create new table
+const User = sequelize.define(
+  "user",
+  {
     id: {
-        type: Sequelize.DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: Sequelize.DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     username: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: Sequelize.DataTypes.STRING
+      type: Sequelize.DataTypes.STRING,
     },
     age: {
-        type: Sequelize.DataTypes.INTEGER,
-        defaultValue: 25
-    }
-},
-    {
-        freezeTableName: true,
-        timestamps: false
-    })
+      type: Sequelize.DataTypes.INTEGER,
+      defaultValue: 25,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
 
-User.sync({ force: true }).then((data) => {
-    console.log('Table and model synced successful', data)
-}).catch((err) => {
-    console.log('Error syncing the table and model', err)
-})
+// Basic Syncing of table
+
+User.sync({ alter: true }) // can be use { force: true } for drop table if exist & create new table with new definition (OPTIONAL)
+  //{ alter: true } for table if exist or create new table
+  .then((data) => {
+    console.log("Table and model synced successful", data);
+  })
+  .catch((err) => {
+    console.log("Error syncing the table and model", err);
+  });
