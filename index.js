960ba34c8c465ddb2cc89f3752f7102dbecacc60
Darshana <keerthi.mac@gmail.com>
2022-01-03 08:28:50 +0530

    update methods

--- index.js
+++ index.js
@@ -64,11 +64,60 @@ const User = sequelize.define(
 
 // Basic Syncing of table
 
-User.sync({ alter: true }) // can be use { force: true } for drop table if exist & create new table with new definition (OPTIONAL)
-  //{ alter: true } for table if exist or create new table
-  .then((data) => {
-    console.log("Table and model synced successful", data);
+// Method 01
+//Using build method to add data to database
+
+// User.sync({ alter: true })
+//   .then(() => {
+//     const user = User.build({
+//       username: "John",
+//       password: "12345",
+//       age: 25,
+//     });
+//     //build method use for if there any condition add before save like validation or any other condition these can be added before .save method
+
+//     return user.save();
+//     //save method is used to save the data in database
+//     //return used for chaining the promise
+//   })
+//   .then((data) => {
+//     console.log(data.toJSON());
+//   })
+
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Method 02
+// use create method to add data to database
+
+User.sync({ alter: true })
+  .then(() => {
+    return User.create({
+      username: "John",
+      password: "12345",
+      age: 25,
+    });
+    //return used for chaining the promise
+    //create method is used to add data to database no need to .save method
   })
+  .then((data) => {
+    //console.log(data.toJSON());
+    // data.username = "John Doe"; //if we want to update data in previous block, it can be done in this stage also
+    // data.age = 30;
+    //data.decrement({ age: 2 }); //decrement method is used to decrement the value of integer
+    data.increment({ age: 2 }); //increment method is used to increment the value of integer
+
+    //return data.save(); // save method is used to save the data in database
+    //return data.destroy(); // destroy method is used to delete the data from database
+    //return data.reload(); // reload method is used to update recode with original data. not updated in this block
+    // return data.save({fields : ['username']}); // fields attribute is used to update only specific field in database
+  })
+  // .then((data) => {
+  //   console.log("Data updated");
+  //   console.log(data.toJSON());
+  // })
+
   .catch((err) => {
-    console.log("Error syncing the table and model", err);
+    console.log(err);
   });
