d345da7a6698ad22d9f7df003ae6cf6509e1578f
Darshana <keerthi.mac@gmail.com>
2022-07-15 08:07:31 +0530

    little update

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../models");
+const { Op } = require("sequelize");
 const sequelize = db.sequelize;
 const User = db.user;
 
@@ -7,28 +8,49 @@ const User = db.user;
 // @desc Get all the element in table
 // @route GET/api/find_all
 const findAllMethod = asyncHandler(async (req, res) => {
-  //   const user = await User.findAll(); //return all the data in table
+  //return all the data in table
+  const findAll = await User.findAll();
 
-  //   const user = await User.findAll({
-  //     attributes: [
-  //       ["username", "username"],
-  //       ["password", "pwd"],
-  //     ],
-  //   }); // return data from specific specific column // 2nd argument is in the array is column heading ('alias')
+  // return data from specific specific column // 2nd argument is in the array is column heading ('alias')
+  const attributes = await User.findAll({
+    attributes: [
+      ["username", "username"],
+      ["password", "pwd"],
+    ],
+  });
 
-  //   const user = await User.findAll({
-  //     attributes: [[sequelize.fn("SUM", sequelize.col("age")), "howOld"]],
-  //   }); // aggregate functions can be run like this "SUM" "AVG" "MAX" "MIN" "COUNT"
+  // aggregate functions can be run like this "SUM" "AVG" "MAX" "MIN" "COUNT"
+  const minMaxCount = await User.findAll({
+    attributes: [[sequelize.fn("SUM", sequelize.col("age")), "howOld"]],
+  });
 
-  //   const user = await User.findAll({ attributes: { exclude: ["password"] } }); // exclude attribute is used to exclude specific column from database
+  // exclude attribute is used to exclude specific column from database
+  const exclude = await User.findAll({ attributes: { exclude: ["password"] } });
 
-  //   const user = await User.findAll({ where: { username: "John", age: 25 } }); // where attribute is used to filter data from database
+  // where attribute is used to filter data from database
+  const where1 = await User.findAll({ where: { username: "John", age: 25 } });
 
-  //   const user = await User.findAll({ limit: 2, offset: 5 }); // limit and offset attribute is used to limit the data from database
+  // limit and offset attribute is used to limit the data from database
+  const limitNoffset = await User.findAll({ limit: 2, offset: 5 });
 
-  //   const user = await User.findAll({ order: [["age", "DESC"]] }); // order attribute is used to sort the data from database
-  //   const user = await User.findAll({ order: [["age", "ASC"]] });
-  res.status(200).json(user);
+  // order attribute is used to sort the data from database
+  const orderDesc = await User.findAll({ order: [["age", "DESC"]] });
+  const orderAsc = await User.findAll({ order: [["age", "ASC"]] });
+
+  //Using operators like OR, AND, NOT, BETWEEN, IN, etc.
+  const operators1 = await User.findAll({
+    where: { [Op.or]: { username: "jhon", age: 25 } },
+  });
+
+  //BETWEEN operator is used to filter data from database
+  const operators2 = await User.findAll({
+    where: { age: { [Op.between]: [25, 30] } },
+  });
+
+  // GREATER operator is used to filter data from database
+  const operators3 = await User.findAll({ where: { age: { [Op.gt]: 25 } } });
+
+  res.status(200).json(minMaxCount);
 });
 
 module.exports = {
